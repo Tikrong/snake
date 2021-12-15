7574c4c1896616b35d0a33f1498d5fb262242d21
Tikrong <60819120+Tikrong@users.noreply.github.com>
2021-12-16 01:33:41 +1000

    Added movement and food

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 # FOR LATER
 # change size-1 and size-2 for -1 and -2, it'll be the same
 
+import random
+
 # class for handling the snake
 class Snake():
     def __init__(self, size, initialPos):
@@ -8,7 +10,7 @@ class Snake():
         # position of the snake, cell[size-1] is head
         self.cells = []
         # position where the tail was before it was deleted (after snake moved)
-        self.tailWasHere = (0,0)
+        self.tailWasHere = (1,1)
         for i in range(self.size):
             self.cells.append((initialPos[0], initialPos[1] - size + 1 + i))
             print(self.cells)
@@ -46,17 +48,23 @@ class Snake():
 
     # Growth
     def Grow(self):
-        self.cells.append(self.tailWasHere)
+        self.cells.insert(0, self.tailWasHere)
         self.size += 1
 
 
 
 # class for handling the game board
 
+# gamefield is an 2d array where
+# 0 - empty cell
+# 1 - snake
+# 2 - obstacle or wall
+# 3 - food
+
 class GameField():
     def __init__(self):
-        self.height = 5
-        self.width = 5
+        self.height = 10
+        self.width = 10
         # initiate game field
         self.field = []
         for y in range(self.height):
@@ -66,45 +74,64 @@ class GameField():
             self.field.append(row)
         # add borders to the field
         self.borders = set()
-        # add vercical borders
+        # add horizontal borders to the set of obstacles and to the gamefield
         for x in range(self.width):
             self.borders.add((0,x))
             self.borders.add((self.height-1, x))
-        # add horizontal borders
+            self.field[0][x] = 2
+            self.field[self.height-1][x] = 2
+        # add horizontal borders to the set of obstacles and to the gamefield
         for x in range(self.height):
             self.borders.add((x,0))
             self.borders.add((x, self.width-1))
+            self.field[x][0] = 2
+            self.field[x][self.width-1] = 2
+        
+        # for storing information about food
+        self.food = set()
 
+    # check whether snake make a valid move, i.e. not collided with a wall or itself
+    # returns true if snake is collided or false if it is alive
+    def IsSnakeCollided(self, snake):
+        # check whether snake has collided with a wall
+        for i in range(snake.size):
+            if snake.cells[i] in  self.borders:
+                return True
+        
+        # check whether snake has collided with itself
+        if len(set(snake.cells)) < snake.size:
+            return True
+        
+        return False
 
-        # add
-
+    # this 
     def DrawField(self, _snake):
         for i in range(_snake.size):
             self.field[_snake.cells[i][0]][_snake.cells[i][1]] = 1
         # mark as empty the position where tail was before the movement
         self.field[_snake.tailWasHere[0]][_snake.tailWasHere[1]] = 0
 
+    # method for placing food in the field
+    def PlaceFood(self):
+        while True:
+            food = (random.randint(0, self.height-1), random.randint(0, self.width-1))
+            if food not in self.borders and food not in self.food:
+                self.food.add(food)
+                self.field[food[0]][food[1]] = 3
+                break
+    # checks whether snake got food
+    def DidSnakeGetFood(self, snake):
+        if snake.cells[snake.size-1] in self.food:
+            self.food.remove(snake.cells[snake.size-1])
+            self.field[snake.cells[snake.size-1][0]][snake.cells[snake.size-1][1]] = 1
+            snake.Grow()
+            self.DrawField(snake)
+            return True
+        else:
+            return False
 
 
-game = GameField()
-snake = Snake(3, (3,3))
-game.DrawField(snake)
-for row in game.field:
-    print(row)
 
-print()
-
-snake.MoveUp()
-game.DrawField(snake)
-snake.MoveOnYourOwn()
-
-game.DrawField(snake)
-for row in game.field:
-    print(row)
-
-print()
-
-print(game.borders)
 
 
 
--- snake.py
+++ snake.py
@@ -1,20 +1,28 @@
-import  pygame
+import pygame
+from pygame import locals
 from helpers import*
 
 pygame.init()
 
+clock = pygame.time.Clock()
+
+# we track how much time passed since last movements and if snake didn't moved we move it
+timeSinceLastMovement = 0
+MoveEveryMilliseconds = 1000
+
 #screen size
-screen_height = 600
-screen_width = 450
+screen_height = 500
+screen_width = 300
 
 # colors
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 GREEN = (0,255,0)
 RED = (255,0,0)
+YELLOW = (255,255,0)
 
 # sizes
-tile_size = 50
+tile_size = 30
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
@@ -22,21 +30,66 @@ game = GameField()
 snake = Snake(3, (3,3))
 game.DrawField(snake)
 
+game.PlaceFood()
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == locals.K_LEFT:
+                snake.MoveLeft()
+                print("left")
+                game.DrawField(snake)
+                timeSinceLastMovement = 0
+            elif event.key == locals.K_RIGHT:
+                snake.MoveRight()
+                print("right")
+                game.DrawField(snake)
+                timeSinceLastMovement = 0
+            elif event.key == locals.K_UP:
+                snake.MoveUp()
+                print("up")
+                game.DrawField(snake)
+                timeSinceLastMovement = 0
+            elif event.key == locals.K_DOWN:
+                snake.MoveDown()
+                print("down")
+                game.DrawField(snake)
+                timeSinceLastMovement = 0
+    
+    if timeSinceLastMovement > MoveEveryMilliseconds:
+        snake.MoveOnYourOwn()
+        game.DrawField(snake)
+        timeSinceLastMovement = 0
 
-    # drow gamefield
+    # draw gamefield grid, walls, snake
     for i in range(game.height):
         for j in range(game.width):
-            if game.field[i][j] == 1:
+            # if cell is empty draw square with white borders
+            if game.field[i][j] == 0:
                 rect = pygame.Rect(j*tile_size, i*tile_size, tile_size, tile_size)
+                pygame.draw.rect(screen, BLACK, rect, 0)
                 pygame.draw.rect(screen, WHITE, rect, 1)
+            elif game.field[i][j] == 1:
+                rect = pygame.Rect(j*tile_size, i*tile_size, tile_size, tile_size)
+                pygame.draw.rect(screen, GREEN, rect, 0)
+            elif game.field[i][j] == 2:
+                rect = pygame.Rect(j*tile_size, i*tile_size, tile_size, tile_size)
+                pygame.draw.rect(screen, RED, rect, 0)
+            elif game.field[i][j] == 3:
+                rect = pygame.Rect(j*tile_size, i*tile_size, tile_size, tile_size)
+                pygame.draw.rect(screen, YELLOW, rect, 0)
 
-
+    if game.IsSnakeCollided(snake):
+        print("GAME OVER")
+    
+    game.DidSnakeGetFood(snake)
+        
+   
     pygame.display.flip()
+    timeSinceLastMovement += clock.tick(30)
 
 
 
